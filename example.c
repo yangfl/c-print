5a88a94b2632e7761a4f4232d9fd17d2c3568ab0
yangfl <yangfl@users.noreply.github.com>
2020-07-03 17:48:23 +0800

    update example

--- example.c
+++ example.c
@@ -1,12 +1,22 @@
 #include "print.h"
 
 
+int int_f() {
+  return 2;
+}
+
+
+char *string_f() {
+  return "test";
+}
+
+
 int main() {
   char *str = "aaa";
   int i = 9;
   float f = .1;
 
-  print(_"Hello", _"world!");
-  print(str, i, SEP "\t", END "\n");
-  print(f, 1);
+  print(_"Hello", _"world!", SEP "\t", END "\n\n");
+  print(_"Basic types:", str, i, f, 1);
+  print(_"Function returns:", int_f(), string_f());
 }
